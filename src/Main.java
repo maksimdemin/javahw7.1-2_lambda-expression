a33474b0721fbbc93b78bcb2c6d5b688d5b6a41a
MaksimDemin <mdeminalex@icloud.com>
2019-10-19 23:48:21 +0800

    hw7.1 finished

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main
+{
+    private static String staffFile = "data/staff.txt";
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args)
+    {
+        ArrayList<Employee> staff = loadStaffFromFile();
+
+//        staff.sort((o1, o2) -> o1.getName().compareTo(o2.getName())); // сортировка по алфавиту
+//        staff.sort((o1, o2) -> o1.getSalary().compareTo(o2.getSalary())); // сортировка по ЗП
+
+        staff.sort(Comparator.comparing(Employee::getSalary).thenComparing(Employee::getName)); // сортировка по ЗП и алфавиту
+        staff.forEach(System.out::println);
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile()
+    {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try
+        {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for(String line : lines)
+            {
+                String[] fragments = line.split("\t");
+                if(fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                    fragments[0],
+                    Integer.parseInt(fragments[1]),
+                    (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        }
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
